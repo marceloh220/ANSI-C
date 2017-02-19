a18d75ed4b508e8d4f16628cfe90ef2258835871
Marcelo H Moraes <marceloh220@hotmail.com>
2017-02-18 22:01:32 -0200

    Update adc.h

--- IHM_lcd595/Atmega328/adc.h
+++ IHM_lcd595/Atmega328/adc.h
@@ -14,7 +14,7 @@
 void adc_init(char Vref, char prescale)    {
     
     //For Vref
-	// 0 = Vref in pin Aref
+    // 0 = Vref in pin Aref
     // 1 = Vref is the same of AVcc
     // 2 = No configuration, using 1
     // 3 = Vref int mcu of 1.1V
@@ -22,13 +22,13 @@ void adc_init(char Vref, char prescale)    {
     
     //For prescale
     // 0 = 1/2
-	// 1 = 1/2
-	// 2 = 1/4
-	// 3 = 1/8
-	// 4 = 1/16
-	// 5 = 1/32
-	// 6 = 1/64
-	// 7 = 1/128
+    // 1 = 1/2
+    // 2 = 1/4
+    // 3 = 1/8
+    // 4 = 1/16
+    // 5 = 1/32
+    // 6 = 1/64
+    // 7 = 1/128
     
     cli();
     if(Vref > 3 || Vref == 2) Vref = 1;
